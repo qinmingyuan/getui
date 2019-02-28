bfce4a86762f9216b83b33cc80cea88b4c318031
qinmingyuan <mingyuan0715@foxmail.com>
2019-02-28 18:42:23 +0800

    link

--- lib/getui/message/transmission.rb
+++ lib/getui/message/transmission.rb
@@ -1,13 +1,17 @@
 module Getui
   class Message
     class Transmission < Getui::Message
-      attr_writer :transmission_type, :transmission_content, :apns
+      attr_writer :transmission_type, :transmission_content
 
-      def initialize(transmission_content, is_offline: true, offline_expire_time: 7 * 60 * 60 * 24, push_network_type: 0, **options)
+      def initialize(transmission_content = nil, is_offline: true, offline_expire_time: 7 * 60 * 60 * 24, push_network_type: 0, apns: nil, **options)
         super('transmission', is_offline: is_offline, offline_expire_time: offline_expire_time, push_network_type: push_network_type)
         @transmission_type = false
-        @transmission_content = transmission_content
-        @apns = options[:apns]
+        @apns = apns
+        if transmission_content.present?
+          @transmission_content = transmission_content
+        else
+          @transmission_content = @apns.as_json.to_json
+        end
       end
 
       def as_json
